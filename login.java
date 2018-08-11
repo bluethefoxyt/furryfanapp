3e3610c0c8ced9447a05aeece53292be86a71ccd
bluethefox <bluethefoxoffical@gmail.com>
2018-08-11 18:54:05 +0100

    second commit

--- login.java
+++ login.java
@@ -1,14 +1,28 @@
 package cf.furryfan.furryfanapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
     RelativeLayout Relay1;
+
 
     Handler handler = new Handler();
 
@@ -19,11 +33,24 @@ public class login extends AppCompatActivity {
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         //animation of login screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SharedPreferences mSettings = getApplicationContext().getSharedPreferences("Login", MODE_PRIVATE);
+        if (mSettings.getString("loginsuccess", "0").equals("1")) {
+
+
+            startActivity(new Intent(getApplicationContext(), usershome.class));
+
+        } else {
+
+
+        }
         Relay1 = findViewById(R.id.Relay1);
         handler.postDelayed(runnable, 2000);
 
@@ -35,7 +62,49 @@ public class login extends AppCompatActivity {
 
     //login verification code
     public void login() {
+        final SharedPreferences mSettings = getApplicationContext().getSharedPreferences("Login", MODE_PRIVATE);
+        final TextView tv = findViewById(R.id.logintt);
+        final EditText username = findViewById(R.id.utb);
+        final EditText password = findViewById(R.id.ptb);
+        StringRequest postRequest = new StringRequest(Request.Method.POST, "https://furryfan.cf/app/login.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        if (response.equals("1")) {
+                            SharedPreferences.Editor editor;
+                            editor = mSettings.edit();
+                            editor.putString("loginsuccess", "1");
+                            editor.putString("username", username.getText().toString());
+                            editor.commit();
+                            startActivity(new Intent(getApplicationContext(), usershome.class));
 
+                        } else {
+
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        password.setBackgroundColor(4);
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username.getText().toString());
+                params.put("password", password.getText().toString());
+
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(postRequest);
     }
 
 
---
+++ usershome.java
@@ -0,0 +1,13 @@
+package cf.furryfan.furryfanapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class usershome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usershome);
+    }
+}
