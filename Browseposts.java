44cbb8a49f3023f04773bc2de80513fab98c5120
bluethefox <bluethefoxoffical@gmail.com>
2018-08-24 21:30:36 +0100

    added features 1p/1p

--- Browseposts.java
+++ Browseposts.java
@@ -28,11 +28,7 @@ public class Browseposts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browseposts);
         recyclerView = findViewById(R.id.cvr);
-
-
         listItems = new ArrayList<>();
-
-
         loadRecyclerViewData();
     }
 
@@ -44,7 +40,7 @@ public class Browseposts extends AppCompatActivity {
         progressDialog.show();
         StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://furryfan.cf/app/browseview.php", new Response.Listener<String>() {
 
-            
+
             @Override
             public void onResponse(String s) {
                 progressDialog.dismiss();
